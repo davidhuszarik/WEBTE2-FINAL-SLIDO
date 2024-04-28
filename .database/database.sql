38b868bdfa0cc1e4952ce3f86baab81cc492c8aa
Gergely Tóth <102431122+GergelyToth-stuba@users.noreply.github.com>
2024-04-28 22:52:09 +0200

    Added user table and event for removal of access token

--- .database/database.sql
+++ .database/database.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1deb1+jammy2
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Mar 21, 2024 at 09:17 PM
--- Server version: 8.0.36-0ubuntu0.22.04.1
--- PHP Version: 8.3.4
+-- Host: mysql
+-- Generation Time: Apr 28, 2024 at 08:44 PM
+-- Server version: 8.0.32
+-- PHP Version: 8.2.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,9 +20,62 @@ SET time_zone = "+00:00";
 --
 -- Database: `final`
 --
-DROP DATABASE IF EXISTS `final`;
 CREATE DATABASE IF NOT EXISTS `final` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `final`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` bigint NOT NULL,
+  `username` varchar(64) COLLATE utf8mb4_general_ci NOT NULL,
+  `hashed_password` char(64) COLLATE utf8mb4_general_ci NOT NULL,
+  `salt` char(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT ' spVj3cU',
+  `google_2FA_secret` varchar(21) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `access_token` int DEFAULT NULL,
+  `last_access` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Truncate table before insert `user`
+--
+
+TRUNCATE TABLE `user`;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` bigint NOT NULL AUTO_INCREMENT;
+
+DELIMITER $$
+--
+-- Events
+--
+DROP EVENT IF EXISTS `clearOldAccessTokens`$$
+CREATE DEFINER=`Tim5`@`%` EVENT `clearOldAccessTokens` ON SCHEDULE EVERY 1 MINUTE STARTS '2024-04-28 22:00:00' ON COMPLETION NOT PRESERVE ENABLE DO UPDATE `user`
+SET access_token = NULL
+WHERE TIMESTAMPDIFF(HOUR, last_access, NOW()) >= 24$$
+
+DELIMITER ;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
