ab9747bf6c1b79192c2856157f65c6a3213cb69a
Gergely Tóth <102431122+GergelyToth-stuba@users.noreply.github.com>
2024-05-08 15:43:30 +0200

    Added quotation marks for ports in docker file

--- .database/final.sql
+++ .database/final.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql
--- Generation Time: May 07, 2024 at 03:55 PM
+-- Generation Time: May 08, 2024 at 01:40 PM
 -- Server version: 8.0.32
 -- PHP Version: 8.2.8
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `final`
 --
+CREATE DATABASE IF NOT EXISTS `final` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `final`;
 
 -- --------------------------------------------------------
 
@@ -27,12 +29,14 @@ SET time_zone = "+00:00";
 -- Table structure for table `answers`
 --
 
+DROP TABLE IF EXISTS `answers`;
 CREATE TABLE `answers` (
   `id` bigint NOT NULL,
   `period_id` bigint NOT NULL,
-  `user_id` bigint NOT NULL,
+  `user_id` bigint DEFAULT NULL,
   `type` enum('single_choice','multi_choice','open') NOT NULL,
-  `free_answere` text NOT NULL
+  `free_answer` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `vote_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -41,6 +45,7 @@ CREATE TABLE `answers` (
 -- Table structure for table `options`
 --
 
+DROP TABLE IF EXISTS `options`;
 CREATE TABLE `options` (
   `id` bigint NOT NULL,
   `question_id` bigint NOT NULL,
@@ -55,18 +60,18 @@ CREATE TABLE `options` (
 -- Table structure for table `periods`
 --
 
+DROP TABLE IF EXISTS `periods`;
 CREATE TABLE `periods` (
   `id` bigint NOT NULL,
-  `question_id` bigint NOT NULL,
+  `question_id` bigint DEFAULT NULL,
   `title_en` varchar(64) NOT NULL,
   `title_sk` varchar(64) NOT NULL,
   `content_en` text NOT NULL,
   `content_sk` text NOT NULL,
   `type` enum('single_choice','multi_choice','open') NOT NULL,
-  `start_timestamp` timestamp NOT NULL,
-  `end_timestamp` timestamp NOT NULL,
-  `code` char(6) NOT NULL,
-  `qr_code` varchar(2048) NOT NULL
+  `start_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `end_timestamp` timestamp NOT NULL COMMENT 'This must be set a adequately large timestamp from start timestamp by default if not set',
+  `code` char(6) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -75,10 +80,10 @@ CREATE TABLE `periods` (
 -- Table structure for table `picked_static_options`
 --
 
+DROP TABLE IF EXISTS `picked_static_options`;
 CREATE TABLE `picked_static_options` (
   `answer_id` bigint NOT NULL,
-  `static_option_id` bigint NOT NULL,
-  `vote_time` timestamp NOT NULL
+  `static_option_id` bigint NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -87,6 +92,7 @@ CREATE TABLE `picked_static_options` (
 -- Table structure for table `questions`
 --
 
+DROP TABLE IF EXISTS `questions`;
 CREATE TABLE `questions` (
   `id` bigint NOT NULL,
   `user_id` bigint NOT NULL,
@@ -94,7 +100,7 @@ CREATE TABLE `questions` (
   `title_sk` varchar(64) NOT NULL,
   `content_en` text NOT NULL,
   `content_sk` text NOT NULL,
-  `creation_date` timestamp NOT NULL,
+  `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `type` enum('single_choice','multi_choice','open') NOT NULL DEFAULT 'single_choice',
   `is_open` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -105,6 +111,7 @@ CREATE TABLE `questions` (
 -- Table structure for table `static_options`
 --
 
+DROP TABLE IF EXISTS `static_options`;
 CREATE TABLE `static_options` (
   `id` bigint NOT NULL,
   `period_id` bigint NOT NULL,
@@ -119,16 +126,17 @@ CREATE TABLE `static_options` (
 -- Table structure for table `user`
 --
 
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` bigint NOT NULL,
   `username` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `hashed_password` char(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `salt` char(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT ' spVj3cU',
+  `salt` char(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'spVj3cUa',
   `google_2FA_secret` varchar(21) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `access_token` varchar(16) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `access_token` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `last_access` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `role` enum('admin','user') COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'user'
+  `role` enum('admin','user') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'user'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -161,6 +169,7 @@ ALTER TABLE `periods`
 -- Indexes for table `picked_static_options`
 --
 ALTER TABLE `picked_static_options`
+  ADD PRIMARY KEY (`answer_id`,`static_option_id`),
   ADD KEY `fk_pickedStaticOption_answer` (`answer_id`),
   ADD KEY `fk_pickedStaticOption_optionStatic` (`static_option_id`);
 
@@ -234,44 +243,45 @@ ALTER TABLE `user`
 -- Constraints for table `answers`
 --
 ALTER TABLE `answers`
-  ADD CONSTRAINT `fk_answer_period` FOREIGN KEY (`period_id`) REFERENCES `periods` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_answer_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_answer_period` FOREIGN KEY (`period_id`) REFERENCES `periods` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  ADD CONSTRAINT `fk_answer_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE RESTRICT;
 
 --
 -- Constraints for table `options`
 --
 ALTER TABLE `options`
-  ADD CONSTRAINT `fk_option_question` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD CONSTRAINT `fk_option_question` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
 
 --
 -- Constraints for table `periods`
 --
 ALTER TABLE `periods`
-  ADD CONSTRAINT `fk_period_question` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD CONSTRAINT `fk_period_question_id` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`) ON DELETE SET NULL ON UPDATE RESTRICT;
 
 --
 -- Constraints for table `picked_static_options`
 --
 ALTER TABLE `picked_static_options`
-  ADD CONSTRAINT `fk_pickedStaticOption_answer` FOREIGN KEY (`answer_id`) REFERENCES `answers` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `fk_pickedStaticOption_optionStatic` FOREIGN KEY (`static_option_id`) REFERENCES `static_options` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD CONSTRAINT `fk_pickedStaticOption_answer` FOREIGN KEY (`answer_id`) REFERENCES `answers` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  ADD CONSTRAINT `fk_pickedStaticOption_optionStatic` FOREIGN KEY (`static_option_id`) REFERENCES `static_options` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
 
 --
 -- Constraints for table `questions`
 --
 ALTER TABLE `questions`
-  ADD CONSTRAINT `fk_question_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD CONSTRAINT `fk_question_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
 
 --
 -- Constraints for table `static_options`
 --
 ALTER TABLE `static_options`
-  ADD CONSTRAINT `fk_staticOption_period` FOREIGN KEY (`period_id`) REFERENCES `periods` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD CONSTRAINT `fk_staticOption_period` FOREIGN KEY (`period_id`) REFERENCES `periods` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
 
 DELIMITER $$
 --
 -- Events
 --
+DROP EVENT IF EXISTS `clearOldAccessTokens`$$
 CREATE DEFINER=`Tim5`@`%` EVENT `clearOldAccessTokens` ON SCHEDULE EVERY 1 MINUTE STARTS '2024-04-28 22:00:00' ON COMPLETION NOT PRESERVE ENABLE DO UPDATE `user`
 SET access_token = NULL
 WHERE TIMESTAMPDIFF(HOUR, last_access, NOW()) >= 24$$
