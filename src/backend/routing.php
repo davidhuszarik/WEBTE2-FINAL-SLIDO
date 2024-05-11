6ca57a50235f99326af8d3f0272f33a51aa051f3
Denis <denispudelka@gmail.com>
2024-05-11 16:52:11 +0200

    Temp.

--- src/backend/routing.php
+++ src/backend/routing.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . "/controllers/LoginController.php";
+
 use Controllers\LoginController;
 
 error_reporting(E_ALL);
@@ -7,25 +8,60 @@ ini_set('display_errors', 1);
 
 ob_start();
 
-// header("Content-Type: application/json");
+header("Content-Type: application/json");
 
 $method = $_SERVER['REQUEST_METHOD'];
 $uri_parts = explode('?', $_SERVER['REQUEST_URI'], 2);
 $endpoint = $uri_parts[0];
+$query_string = isset($uri_parts[1]) ? $uri_parts[1] : null;
+parse_str($query_string, $params);
+
+//$connection = DatabaseConnection::getInstance()->getConnection();
+//print_r($connection);
 
 //echo "Method: " . $method . "\n";
 //echo "Path info: " . $endpoint . "\n";
 
-if(str_starts_with($endpoint, "/api/test"))
-{
-//    if($endpoint == "/api/test"){
-//
-//    }
-//}
-else if($endpoint == "/login")
-{
+if (str_starts_with($endpoint, "/api")) {
+    switch ($endpoint) {
+        case "/api/question":
+            echo "Method: " . $method . "\n";
+            echo "Path info: " . $endpoint . "\n";
+            switch ($method) {
+                case "GET":
+                    if (isset($params['id'])) {
+                        echo "Params: " . $params['id'] . "\n";
+                        // TODO: Retrieve question by id
+                    } elseif(isset($params['code'])) {
+                        echo "Params: " . $params['code'] . "\n";
+                        // TODO: Retrieve question by code
+                    }else{
+                        // TODO: Retrieve all questions
+                    }
+                    break;
+                case "POST":
+                    // TODO: Create new question
+                    break;
+                default:
+                    http_response_code(405);
+                    echo json_encode(["error" => "Method not allowed"]);
+                    break;
+            }
+            break;
+        case "/api/option":
+            // Handle option-related requests
+            break;
+        default:
+            http_response_code(404);
+            $response = new stdClass();
+            $response->code = 404;
+            $response->message = "Not Found";
+            $response->description = "The request resource was not found on this server";
+            echo json_encode($response);
+    }
+} else if ($endpoint == "/login") {
     $controller = new LoginController();
-    switch($method){
+    switch ($method) {
         case "GET":
             $controller->index();
             break;
