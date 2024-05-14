b7dd2ad9d10683569c5be0fa440efb5f4a801c31
Tgxxxxx <xtothg@stuba.sk>
2024-05-15 00:22:07 +0200

    Added test commented out test code for frontend

--- src/backend/views/openAnswer.php
+++ src/backend/views/openAnswer.php
@@ -4,3 +4,24 @@ echo json_encode(["period" => $period]);
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.3.0/jquery.form.min.js"></script>
+
+<script>
+    /*
+    $.ajax({
+        url: window.location.href,
+        type: "POST",
+        data: {
+            free_answer: "asdasdasd",
+        },
+        success: function(response) {
+            // Handle success
+            console.log("POST request successful");
+            console.log("Response:", response);
+        },
+        error: function(xhr, status, error) {
+            // Handle errors
+            console.error("Error making POST request:", error);
+        }
+    });
+    */
+</script>
