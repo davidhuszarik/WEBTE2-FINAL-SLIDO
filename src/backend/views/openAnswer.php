f21e974067da519ee54336e5ebb6870217a0ca65
Tgxxxxx <xtothg@stuba.sk>
2024-05-15 00:15:04 +0200

    Added ajax for testing to views

--- src/backend/views/openAnswer.php
+++ src/backend/views/openAnswer.php
@@ -1,2 +1,6 @@
 <?php
 echo json_encode(["period" => $period]);
+?>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.3.0/jquery.form.min.js"></script>
